cc87c2c2319f2c7a6799090b94467955ba03290e
sacrmr <106908810+sacrmr@users.noreply.github.com>
2023-02-22 11:46:22 +0100

    Added js function using moment js to get time

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = `${parisTime.format(
+    "h:mm.ss [<small>]A[</small>]"
+  )}`;
+
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = `${londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
