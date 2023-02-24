f18494fb686db13d9ad9f504c83b475a4624d808
sacrmr <106908810+sacrmr@users.noreply.github.com>
2023-02-24 10:28:56 +0100

    Added emojis to select

--- javascript/index.js
+++ javascript/index.js
@@ -28,7 +28,7 @@ function updateCity(event) {
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-  let cityName = cityTimeZone.replace("_", "").split("/")[1];
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
 
