8778e73bc96354116571c37d32851cfcad072745
sacrmr <106908810+sacrmr@users.noreply.github.com>
2023-02-24 10:44:16 +0100

    Added additional city to cities

--- javascript/index.js
+++ javascript/index.js
@@ -21,6 +21,18 @@ function updateTime() {
       "h:mm.ss [<small>]A[</small>]"
     )}`;
   }
+
+  let newYorkElement = document.querySelector("#new_york");
+
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = `${newYorkTime.format(
+      "h:mm.ss [<small>]A[</small>]"
+    )}`;
+  }
 }
 
 function updateCity(event) {
